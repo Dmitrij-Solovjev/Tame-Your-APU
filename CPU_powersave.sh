c11d953a3cb467d39057fdcdcdba40467c19cfbb
Dmitrij-Solovjev <90474877+Dmitrij-Solovjev@users.noreply.github.com>
2023-12-30 20:25:55 +0300

    Delete CPU_powersave.sh

--- CPU_powersave.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Configure processor
-# https://github.com/FlyGoat/RyzenAdj
-
-
-ryzenadj --tctl-temp=60 --slow-limit=8000 --fast-limit=12000 --gfx-clk=200
-#notify-send "Включён энергоэффективный режим"
-powerprofilesctl set power-saver
